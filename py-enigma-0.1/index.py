97ea5e8ae5e39cf0836301266bce8bfaaf2eef05
kvguarin <kvguarin@dons.usfca.edu>
2017-12-04 14:30:47 -0800

    added the simulator page

--- py-enigma-0.1/index.py
+++ py-enigma-0.1/index.py
@@ -9,5 +9,27 @@ app.debug = True
 def landingPage():
     return render_template('index.html')
 
+@app.route('/simulator', methods=['POST'])
+def simulator():
+    html = ""
+    html += "<html>\n"
+    html += "<body>\n"
+    html += '<form method = "POST" action="form_input">\n'
+    html += 'Enter name of movie: <input type="text" name="userInput" />\n'
+    html += '<input type="submit" value="submit" />\n'
+    html += "</form>\n"
+    html += "</body>\n"
+    html += "</html>\n"
+    return html
+
+@app.route("/decryptResult")
+def decryptResult():
+    return 1
+
+@app.route("/encryptResult")
+def encryptResult():
+    return 1
+
+
 if __name__ =="__main__":
     app.run()
