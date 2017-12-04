8e2640b43f0ba469fccdfc89e01067effb89687e
kvguarin <kvguarin@dons.usfca.edu>
2017-12-04 14:48:41 -0800

    adding decrypt

--- py-enigma-0.1/index.py
+++ py-enigma-0.1/index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
@@ -11,22 +12,25 @@ def landingPage():
 
 @app.route('/simulator', methods=['POST'])
 def simulator():
-    html = ""
-    html += "<html>\n"
-    html += "<body>\n"
-    html += '<form method = "POST" action="form_input">\n'
-    html += 'Enter name of movie: <input type="text" name="userInput" />\n'
-    html += '<input type="submit" value="submit" />\n'
-    html += "</form>\n"
-    html += "</body>\n"
-    html += "</html>\n"
+    html = "hello"
+    # html += "<html>\n"
+    # html += "<body>\n"
+    # html += '<form method = "POST" action="form_input">\n'
+    # html += 'Enter name of movie: <input type="text" name="userInput" />\n'
+    # html += '<input type="submit" value="submit" />\n'
+    # html += "</form>\n"
+    # html += "</body>\n"
+    # html += "</html>\n"
     return html
 
-@app.route("/decryptResult")
+@app.route("/decryptResult", methods=['POST'])
 def decryptResult():
+    rotor1 = request.form['rotor1']
+
+
     return 1
 
-@app.route("/encryptResult")
+@app.route("/encryptResult", methods=['POST'])
 def encryptResult():
     return 1
 
